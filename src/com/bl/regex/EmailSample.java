b7d51ad3868d248dd1597090c0cd1b502bbc35b9
mohini patange <patangemohini5@gmail.com>
2022-06-09 22:41:29 +0530

    Added : valid and invalid email samples provide saparatly

---
+++ src/com/bl/regex/EmailSample.java
@@ -0,0 +1,48 @@
+package com.bl.regex;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EmailSample {
+
+    public void checkSampleEmail() {
+        ArrayList<String> emailSamples = new ArrayList<>();
+
+        emailSamples.add("abc@yahoo.com");
+        emailSamples.add("abc-100@yahoo.com");
+        emailSamples.add("abc.100@yahoo.com");
+        emailSamples.add("abc111@abc.com");
+        emailSamples.add("abc-100@abc.net");
+        emailSamples.add("abc.100@abc.com.au");
+        emailSamples.add("abc@1.com");
+        emailSamples.add("abc@gmail.com.com");
+        emailSamples.add("abc+100@gmail.com");
+
+        emailSamples.add("abc");
+        emailSamples.add("abc@.com.my");
+        emailSamples.add("abc123@gmail.a");
+        emailSamples.add("abc123@.com");
+        emailSamples.add("abc123@.com.com");
+        emailSamples.add(".abc@abc.com");
+        emailSamples.add("abc()*@gmail.com");
+        emailSamples.add("abc@%*.com");
+        emailSamples.add("abc..2002@gmail.com");
+        emailSamples.add("abc.@gmail.com");
+        emailSamples.add("abc@abc@gmail.com");
+        emailSamples.add("abc@gmail.com.1a");
+        emailSamples.add("abc@gmail.com.aa.au");
+
+        String regex = "^[\\w+_-]+(?:\\.[\\w+_-]+)*[@][\\w]{1,}([.]{1}[a-z]{2,}){1,2}$";
+        Pattern pattern = Pattern.compile(regex);
+
+        for (String str : emailSamples) {
+            Matcher matcher = pattern.matcher(str);
+            if (matcher.matches()) {
+                System.out.println("Valid email : " + str + " : " + matcher.matches());
+            } else {
+                System.out.println("Invalid email : " + str + " : " + matcher.matches());
+            }
+        }
+    }
+}
--- src/com/bl/regex/UserRegistrationMain.java
+++ src/com/bl/regex/UserRegistrationMain.java
@@ -9,6 +9,7 @@ public class UserRegistrationMain {
     static final int EMAIL = 3;
     static final int PHONE_NUMBER = 4;
     static final int PASSWORD = 5;
+    static final int SAMPLE_EMAIL = 6;
 
     public static void main(String[] args) {
         System.out.println("Welcome to user registration program!!");
@@ -17,7 +18,7 @@ public class UserRegistrationMain {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter choice :");
         int option = scanner.nextInt();
-            
+
         switch (option) {
             case FIRST_NAME:
                 userRegistration.checkFirstName();
@@ -33,6 +34,10 @@ public class UserRegistrationMain {
                 break;
             case PASSWORD:
                 userRegistration.checkValidPassword();
+                break;
+            case SAMPLE_EMAIL:
+                EmailSample emailSample = new EmailSample();
+                emailSample.checkSampleEmail();
                 break;
             default:
                 System.out.println("invalid option!");
