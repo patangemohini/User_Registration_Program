e1a45440f2ad424374f2ab793db6fa1a7a328d04
mohini patange <patangemohini5@gmail.com>
2022-06-09 20:06:23 +0530

    Added : Welcome Message

---
+++ src/com/bl/regex/UserRegistrationMain.java
@@ -0,0 +1,7 @@
+package com.bl.regex;
+
+public class UserRegistrationMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to user registration program!!");
+    }
+}
